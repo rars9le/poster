7c1a97b9b69675f2bb8d671787d1f4012cda5c95
lenon <le9240@gmail.com>
2020-08-06 13:45:56 +0000

    minimagick version

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,7 +26,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #上限変更
-  process :resize_to_limit => [700, 700]
+  process :resize_to_limit => [256, 256]
   #JPGで保存
   process :convert => 'jpg'
   
@@ -36,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [300, 300]
+    process :resize_to_fit => [256, 256]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
